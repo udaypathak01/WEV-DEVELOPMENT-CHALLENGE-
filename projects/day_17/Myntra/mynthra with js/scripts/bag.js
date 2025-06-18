c5bb4bfccd247cb8a05b155e28c261f0e15d9ac7
Uday pathak <pathakuday45@gmail.com>
2025-06-18 15:51:27 +0530

    myntra clone update

---
+++ projects/day_17/Myntra/mynthra with js/scripts/bag.js
@@ -0,0 +1,50 @@
+let bagItemObjects;
+
+onLoad();
+
+function onLoad() {
+  loadBagItemObjects();
+  displayBagItem();
+}
+function loadBagItemObjects() {
+  console.log(bagItems);
+
+  bagItemObjects = bagItems.map((itemId) => {
+    for (let i = 0; i < items.length; i++) {
+      if (itemId == items[i].id) {
+        return items[i];
+      }
+    }
+  });
+  console.log(bagItemObjects);
+}
+function displayBagItem() {
+  let containerElement = document.querySelector(".bag-items-container");
+    containerElement.innerHTML =generateHTML(loadBagItemObjects)
+}
+function generateHTML(item) {
+  return `
+<div class="bag-item-container">
+            <div class="item-left-part">
+              <img class="bag-item-img" src="../${item.image}">
+            </div>
+            <div class="item-right-part">
+              <div class="company">${item.company}</div>
+              <div class="item-name">${item.item_name}</div>
+              <div class="price-container">
+                <span class="current-price">Rs ${item.current_price}</span>
+                <span class="original-price">Rs ${item.original_price}</span>
+                <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+              </div>
+              <div class="return-period">
+                <span class="return-period-days">${item.return_period} days</span> return available
+              </div>
+              <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${item.delivery_date}</span>
+              </div>
+            </div>
+
+            <div class="remove-from-cart">X</div>
+          </div>`;
+}
